100bd75d3c3f50ae70a1391d9939de14da0f8e80
farzad <farzadafi40@gmail.com>
2022-06-30 17:29:27 +0430

    fixup! add registerCustomer method

--- customer/src/main/java/com/MicroServiceTest/customer/Controller/CustomerController.java
+++ customer/src/main/java/com/MicroServiceTest/customer/Controller/CustomerController.java
@@ -1,5 +1,17 @@
 package com.MicroServiceTest.customer.Controller;
 
+import com.MicroServiceTest.customer.dto.CustomerRegisterRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("api/customer")
 public class CustomerController {
+
+    @PostMapping("/register")
+    public String registerCustomer(@RequestBody CustomerRegisterRequest request){
+        return "OK";
+    }
 }
