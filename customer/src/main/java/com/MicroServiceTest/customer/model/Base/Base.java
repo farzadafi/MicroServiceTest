5a1f7042d4ea757fd22759857e9ec6c57be28c03
farzad <farzadafi40@gmail.com>
2022-07-01 15:44:51 +0430

    add id field

--- customer/src/main/java/com/MicroServiceTest/customer/model/Base/Base.java
+++ customer/src/main/java/com/MicroServiceTest/customer/model/Base/Base.java
@@ -1,4 +1,35 @@
 package com.MicroServiceTest.customer.model.Base;
 
-public class Base {
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.util.Objects;
+
+@Getter
+@Setter
+@MappedSuperclass
+public class Base<ID extends Integer> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Base<?> that = (Base<?>) o;
+        if (id == null && that.id == null)
+            return false;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
