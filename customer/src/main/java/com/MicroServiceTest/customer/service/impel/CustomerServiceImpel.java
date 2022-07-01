f8f2a8bb51e471c0e114ace42813be6f3bf8ad61
farzad <farzadafi40@gmail.com>
2022-07-01 15:45:19 +0430

    add save method

--- customer/src/main/java/com/MicroServiceTest/customer/service/impel/CustomerServiceImpel.java
+++ customer/src/main/java/com/MicroServiceTest/customer/service/impel/CustomerServiceImpel.java
@@ -1,9 +1,21 @@
 package com.MicroServiceTest.customer.service.impel;
 
 import com.MicroServiceTest.customer.model.Customer;
+import com.MicroServiceTest.customer.repository.CustomerRepository;
 import com.MicroServiceTest.customer.service.CustomerService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustomerServiceImpel implements CustomerService {
 
+    private final CustomerRepository customerRepository;
 
+    public CustomerServiceImpel(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public void save(Customer customer) {
+        customerRepository.save(customer);
+    }
 }
