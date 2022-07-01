15a1be6b0847ce4ee4f544880eb2190794112202
farzad <farzadafi40@gmail.com>
2022-07-01 15:45:07 +0430

    add @Entity to class

--- customer/src/main/java/com/MicroServiceTest/customer/model/Customer.java
+++ customer/src/main/java/com/MicroServiceTest/customer/model/Customer.java
@@ -1,4 +1,25 @@
 package com.MicroServiceTest.customer.model;
 
-public class Customer {
+import com.MicroServiceTest.customer.model.Base.Base;
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Customer extends Base<Integer> {
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
 }
