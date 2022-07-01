d2ed3f8fed6e94cd543e5dc77adfcac94e499c2b
farzad <farzadafi40@gmail.com>
2022-07-01 18:52:48 +0430

    create ExpertApplication for main

---
+++ expert/src/main/java/com/MicroServiceTest/expert/ExpertApplication.java
@@ -0,0 +1,11 @@
+package com.MicroServiceTest.expert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpertApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExpertApplication.class,args);
+    }
+}
