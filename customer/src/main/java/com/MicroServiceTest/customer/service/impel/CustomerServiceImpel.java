ccefb24d159c989359575bdaaa24d552495ca3ea
farzad <farzadafi40@gmail.com>
2022-07-01 10:21:54 +0430

    create CustomerServiceImpel and implement with CustomerService interface

---
+++ customer/src/main/java/com/MicroServiceTest/customer/service/impel/CustomerServiceImpel.java
@@ -0,0 +1,9 @@
+package com.MicroServiceTest.customer.service.impel;
+
+import com.MicroServiceTest.customer.model.Customer;
+import com.MicroServiceTest.customer.service.CustomerService;
+
+public class CustomerServiceImpel implements CustomerService {
+
+
+}
