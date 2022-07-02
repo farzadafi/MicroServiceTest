7139043c343f11d850b7ef5e82f61ad654db9fdb
farzad <farzadafi40@gmail.com>
2022-07-02 06:03:20 +0430

    create ExceptionHandler for controllerAdvice class

---
+++ customer/src/main/java/com/MicroServiceTest/customer/exception/ExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.MicroServiceTest.customer.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+
+@Slf4j
+@ControllerAdvice
+public class ExceptionHandler {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(CustomerNotFoundException.class)
+    public ResponseEntity<String> customerExceptionHandler(CustomerNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+
+}
