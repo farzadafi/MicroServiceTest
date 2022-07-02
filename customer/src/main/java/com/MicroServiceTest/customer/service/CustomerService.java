765510c2e31ad464ebcbf6b70173a053267a5c75
farzad <farzadafi40@gmail.com>
2022-07-02 06:03:40 +0430

    add checkCustomer method

--- customer/src/main/java/com/MicroServiceTest/customer/service/CustomerService.java
+++ customer/src/main/java/com/MicroServiceTest/customer/service/CustomerService.java
@@ -2,6 +2,9 @@ package com.MicroServiceTest.customer.service;
 
 import com.MicroServiceTest.customer.model.Customer;
 
+import java.util.Optional;
+
 public interface CustomerService {
     void save(Customer customer);
+    Optional<Customer> checkCustomer(Integer customerId);
 }
