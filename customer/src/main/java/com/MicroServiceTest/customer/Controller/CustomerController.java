d520f358ef4da4aee44475bb08056e66053b90d1
farzad <farzadafi40@gmail.com>
2022-07-02 06:02:21 +0430

    add checkCustomer method

--- customer/src/main/java/com/MicroServiceTest/customer/Controller/CustomerController.java
+++ customer/src/main/java/com/MicroServiceTest/customer/Controller/CustomerController.java
@@ -1,14 +1,14 @@
 package com.MicroServiceTest.customer.Controller;
 
 import com.MicroServiceTest.customer.dto.CustomerRegisterRequest;
+import com.MicroServiceTest.customer.exception.CustomerNotFoundException;
 import com.MicroServiceTest.customer.model.Customer;
 import com.MicroServiceTest.customer.service.impel.CustomerServiceImpel;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.hibernate.boot.model.naming.IllegalIdentifierException;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.InputMismatchException;
 
 @RestController
 @RequestMapping("api/customer")
@@ -24,6 +24,12 @@ public class CustomerController {
     public String registerCustomer(@Valid @RequestBody CustomerRegisterRequest request){
         Customer customer = new Customer(request.getFirstName(),request.getLastName(),request.getEmail());
         customerServiceImpel.save(customer);
+        return "OK";
+    }
+
+    @GetMapping("checkCustomer/{customerId}")
+    public String checkCustomer(@PathVariable("customerId") Integer customerId){
+        Customer customer = customerServiceImpel.checkCustomer(customerId).orElseThrow(() -> new CustomerNotFoundException("this customer not found"));
         return "OK";
     }
 }
